0dc03126011953801e095f2f9f77d91b99160a58
Aishvarya Sivaramakrishnan <a.sivaramakrishnan@lakeba.com>
2023-04-24 19:10:30 +0530

    Initial commit

---
+++ off-canvas-menu/index.js
@@ -0,0 +1,7 @@
+ document.addEventListener("DOMContentLoaded", function() {
+     const h1 = document.createElement("h1");
+     const text = document.createTextNode("Hello world");
+     h1.appendChild(text);
+     document.body.appendChild(h1);
+
+ });
